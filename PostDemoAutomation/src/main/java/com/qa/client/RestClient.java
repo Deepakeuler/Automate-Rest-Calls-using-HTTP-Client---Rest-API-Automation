e77d5040a3320d3472c8cfd595bd2a87756c95d7
Deepakeuler <deepaksharmaforwork@gmail.com>
2022-12-18 02:34:06 +0530

    getCall

--- PostDemoAutomation/src/main/java/com/qa/client/RestClient.java
+++ PostDemoAutomation/src/main/java/com/qa/client/RestClient.java
@@ -30,18 +30,17 @@ public class RestClient {
         //CloseableHttpResponse is an interface --Executes a request using the default context and processes the response using the
         // given response handler. CloseableHttpResponse.execute(HttpUriRequest request) Executes HTTP request using the default context.
 
+        // Status Code
         int statusCode = closeableHttpResponse.getStatusLine().getStatusCode();
         System.out.println("Status Code is ----->"+statusCode);
 
-        //Now getting response
+        // Json String response
         String responseString = EntityUtils.toString(closeableHttpResponse.getEntity(), "UTF-8");//UTF-8 -> String default charSet
-
         JSONObject responseJson= new JSONObject(responseString);//Converting rresponse string into Json obj
-
         System.out.println("Response Json from API --->"+responseJson);
 
+        //all headers
         Header[] headerArray = closeableHttpResponse.getAllHeaders();
-
         HashMap<String, String> allHeaders = new HashMap<>();
 
         for(int i = 0; i<headerArray.length; i++){
